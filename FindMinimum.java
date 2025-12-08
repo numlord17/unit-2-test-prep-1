7d6d58a3f2f8c0479df6ee91d53684bfe09f1266
numlord17 <numlord17@gmail.com>
2025-12-08 09:50:31 -0800

    Update FindMinimum.java

--- FindMinimum.java
+++ FindMinimum.java
@@ -1,12 +1,24 @@
+import java.util.Random;
+
 public class FindMinimum
 {
    public static void main(String[] args)
    {
-   
+      System.out.print(findMin);
    }
    
    public static int findMin()
    {
-   
+      Random random = new Random();
+      int min = 100;
+      for (int i = 0; i < 10; i++)
+      {
+         int randomNumber = random.nextInt(100);
+         if (randomNumber < min)
+         {
+            min = randomNumber;
+         }
+      }
+      return min;
    }
 }
