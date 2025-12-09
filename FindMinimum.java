e2264ebc5646a65bdd2fb51e5e6751fd02406315
numlord17 <numlord17@gmail.com>
2025-12-09 09:46:51 -0800

    Update FindMinimum.java

--- FindMinimum.java
+++ FindMinimum.java
@@ -4,7 +4,7 @@ public class FindMinimum
 {
    public static void main(String[] args)
    {
-      System.out.print(findMin);
+      System.out.print(findMin());
    }
    
    public static int findMin()
